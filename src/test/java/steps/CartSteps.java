0061dea39a2491b463111ea8a82f1086ea5a8855
Emmanuel Arhu <emmanuelarhu706@gmail.com>
2025-08-29 11:15:41 +0000

    feat(steps): update step definitions for cart interactions

--- src/test/java/steps/CartSteps.java
+++ src/test/java/steps/CartSteps.java
@@ -2,18 +2,21 @@ package steps;
 
 import io.cucumber.java.en.*;
 import utils.TestContext;
+import utils.TestDataManager;
 import static org.junit.Assert.*;
 
 /**
  * Step definitions for Shopping Cart functionality
- * Contains all cart-related step implementations
+ * Uses external test data instead of hardcoded values
  */
 public class CartSteps {
 
     private final TestContext testContext;
+    private final TestDataManager testData;
 
     public CartSteps(TestContext testContext) {
         this.testContext = testContext;
+        this.testData = TestDataManager.getInstance();
     }
 
     @When("I click on the shopping cart")
@@ -21,16 +24,33 @@ public class CartSteps {
         testContext.getProductsPage().clickCartIcon();
     }
 
-    @Then("I should see {string} in the cart")
-    public void i_should_see_item_in_the_cart(String itemName) {
+    @Then("I should see backpack product in the cart")
+    public void i_should_see_backpack_product_in_the_cart() {
+        String itemName = testData.getBackpackName();
         assertTrue("Cart page should be displayed",
                 testContext.getCartPage().isCartPageDisplayed());
         assertTrue("Item should be in cart",
                 testContext.getCartPage().isItemInCart(itemName));
     }
 
-    @When("I remove {string} from cart")
-    public void i_remove_item_from_cart(String itemName) {
+    @Then("I should see bike light product in the cart")
+    public void i_should_see_bike_light_product_in_the_cart() {
+        String itemName = testData.getBikeLightName();
+        assertTrue("Cart page should be displayed",
+                testContext.getCartPage().isCartPageDisplayed());
+        assertTrue("Item should be in cart",
+                testContext.getCartPage().isItemInCart(itemName));
+    }
+
+    @When("I remove backpack product from cart")
+    public void i_remove_backpack_product_from_cart() {
+        String itemName = testData.getBackpackName();
+        testContext.getCartPage().removeItemFromCart(itemName);
+    }
+
+    @When("I remove bike light product from cart")
+    public void i_remove_bike_light_product_from_cart() {
+        String itemName = testData.getBikeLightName();
         testContext.getCartPage().removeItemFromCart(itemName);
     }
 
@@ -63,8 +83,9 @@ public class CartSteps {
                 expectedCount.intValue(), actualCount);
     }
 
-    @Then("I should see the cart page header {string}")
-    public void i_should_see_the_cart_page_header(String expectedHeader) {
+    @Then("I should see the cart page header")
+    public void i_should_see_the_cart_page_header() {
+        String expectedHeader = testData.getCartHeader();
         String actualHeader = testContext.getCartPage().getPageHeader();
         assertEquals("Cart page header should match", expectedHeader, actualHeader);
     }
@@ -86,21 +107,32 @@ public class CartSteps {
                 testContext.getCartPage().isContinueShoppingButtonDisplayed());
     }
 
-    @When("I verify the item {string} price is {string}")
-    public void i_verify_the_item_price_is(String itemName, String expectedPrice) {
+    @When("I verify the backpack product price in cart")
+    public void i_verify_the_backpack_product_price_in_cart() {
+        String itemName = testData.getBackpackName();
+        String expectedPrice = testData.getBackpackPrice();
         String actualPrice = testContext.getCartPage().getItemPrice(itemName);
         assertEquals("Item price should match", expectedPrice, actualPrice);
     }
 
-    @Then("I should not see {string} in the cart")
-    public void i_should_not_see_item_in_the_cart(String itemName) {
+    @When("I verify the bike light product price in cart")
+    public void i_verify_the_bike_light_product_price_in_cart() {
+        String itemName = testData.getBikeLightName();
+        String expectedPrice = testData.getBikeLightPrice();
+        String actualPrice = testContext.getCartPage().getItemPrice(itemName);
+        assertEquals("Item price should match", expectedPrice, actualPrice);
+    }
+
+    @Then("I should not see backpack product in the cart")
+    public void i_should_not_see_backpack_product_in_the_cart() {
+        String itemName = testData.getBackpackName();
         assertFalse("Item should not be in cart",
                 testContext.getCartPage().isItemInCart(itemName));
     }
 
-    @Given("I have {string} in my cart")
-    public void i_have_item_in_my_cart(String itemName) {
-        // This step assumes user is already logged in and on products page
+    @Given("I have backpack product in my cart")
+    public void i_have_backpack_product_in_my_cart() {
+        String itemName = testData.getBackpackName();
         testContext.getProductsPage().addProductToCart(itemName);
         testContext.getProductsPage().clickCartIcon();
         assertTrue("Item should be in cart",
@@ -119,5 +151,19 @@ public class CartSteps {
 
         assertTrue("Cart should be empty",
                 testContext.getCartPage().isCartEmpty());
+    }
+
+    // Keep backward compatibility methods for direct item names
+    @Then("I should see {string} in the cart")
+    public void i_should_see_item_in_the_cart(String itemName) {
+        assertTrue("Cart page should be displayed",
+                testContext.getCartPage().isCartPageDisplayed());
+        assertTrue("Item should be in cart",
+                testContext.getCartPage().isItemInCart(itemName));
+    }
+
+    @When("I remove {string} from cart")
+    public void i_remove_item_from_cart(String itemName) {
+        testContext.getCartPage().removeItemFromCart(itemName);
     }
 }
